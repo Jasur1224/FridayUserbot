e808817e8a13af10897ed553d8b575b94557bf87
Jasur1224 <64468837+Jasur1224@users.noreply.github.com>
2020-07-09 05:15:01 +0000

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,19 +9,19 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_IMG = "https://telegra.ph/file/401f8730c26ad12b8e7e7.jpg"
-ALIVE_caption = "`FRIDAY IS:` **ON-LINE**\n\n"
-ALIVE_caption += "**SYSTEM STATUS**\n\n"
-ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
-ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
+ALIVE_IMG = "https://t.me/HTTP_URL/6"
+ALIVE_caption = "`SHAXSIY ROBOT:` **ON-LINE**\n\n"
+ALIVE_caption += "**ROBOT HOLATI**\n\n"
+ALIVE_caption += "`TELETHON VERSIYASI:` **6.0.9**\n`Python:` **3.7.4**\n\n"
+ALIVE_caption += "`MALUMOTLAR BAZASI:` **FUNKSIYONAL**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
 ALIVE_caption += "**Friday OS** : `3.14`\n\n"
 ALIVE_caption += "**Current Sat** : `StarkGangSat-2.25`\n\n"
 ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
-ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
+ALIVE_caption += "**HEROKU MALUMOTLAR BAZASI** : `AWS - SERVERI ISHLAYAPTI`\n\n"
+ALIVE_caption += "**LITSENZIYA** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
 ALIVE_caption += "Copyright By [StarkGang@Github](GitHub.com/StarkGang)\n\n"
-ALIVE_caption += "[Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+ALIVE_caption += "[ROBOTIMDAN FOYDALANING](https://telegra.ph/FRIDAY-06-15)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
